d9a9022042871460716a2897dbfc36f8ea887cc0
joelulrich <joel.ulrich@gmail.com>
2020-03-22 21:05:15 +1100

    Add files via upload

---
+++ Teams_WAssessment-Setup-Lite.ps1
@@ -0,0 +1,49 @@
+#Network Assessmentsetup script. V3.0 - 22/3/20
+#By Joel Ulrich - julrich@icomm.com.au / joel@julrich.com
+#Setup for Network Assessment script, adds firewall rules and copies binaries
+
+
+#Only change if not installed under default directory
+$nwAssessDir = "C:\Program Files (x86)\Microsoft Skype for Business Network Assessment Tool"
+#how many agents?
+$testUnits = 3
+
+#dont change 
+$workingDir = $PSScriptRoot
+$binDir = $workingDir + "\bin"
+$i = 1
+
+if (!(Test-Path -Path "C:\Program Files (x86)\Microsoft Skype for Business Network Assessment Tool")) {
+    Write-Host "Unable to find Network Assessment Folder under $nwAssessDir"
+    Pause
+}
+
+#check for bindir and creates if needed
+While ($i -lt($testUnits+1)) {
+    if(!(Test-Path -Path ($binDir + "\" + $i.ToString() + "\") ) ) {
+        $binDirDest = $binDir + "\" + $i
+        mkdir $binDirDest
+        Copy-Item -Path ($nwAssessDir + "\*") -Destination $binDirDest -Force
+        
+    }
+    $i++
+}
+
+#Checkbin dir for Tone.wav
+if(!(Test-Path -Path ($binDir + "\Tone.wav") ) ) { 
+    Copy-Item -Path ($nwAssessDir + "\Tone.wav") -Destination $binDir -Force
+    
+}
+
+#Reser int var
+$i = 1
+
+#Add firewall rules
+While ($i -lt($testUnits+1)){
+    $binPath = $binDir + "\" + $i.ToString() + "\" + "NetworkAssessmentTool.exe"
+    New-NetFirewallRule -DisplayName ("Teams Assessment Engine " + $i.ToString()) -Direction Inbound -Program $binPath -Action Allow
+    Write-Host "FW Rule added for $binPath"
+    $i++
+    
+}
+	
